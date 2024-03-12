93ec04b4f7be78831d7d6c4a657db26f904dd006
Elwyn <elwynynion2@gmail.com>
2024-03-13 05:59:16 +0800

    Refactor Sidebar component and update Menus prop

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -13,26 +13,13 @@ function classNames(...classes: string[]) {
 export default function Sidebar({
   open,
   setOpen,
+  Menus,
 }: {
   open: boolean;
   setOpen: (open: boolean) => void;
+  Menus?: any;
 }) {
   const path = usePathname();
-  const Menus = [
-    { title: "🔴 LIVE TV", icon: "MonitorDot", url: "#", gap: "false" },
-    { title: "HISTORY", icon: "History", url: "#" },
-    { title: "LEAGUE", icon: "Hexagon", url: "#" },
-    { title: "RANKINGS", icon: "Trophy", url: "/rankings" },
-    { title: "SPONSORSHIPS", icon: "Joystick", url: "/sponsorship" },
-    { title: "GAMEMASTERS", icon: "UserCog", url: "/gamemasters" },
-    { title: "SHOUTCASTERS", icon: "UsersRound", url: "/shoutcasters" },
-    { title: "FAQ", icon: "Megaphone", url: "/faqs" },
-    {
-      title: "SPIN",
-      icon: "CircleDotDashed",
-      url: "https://spin.battleforsupremacy.tv/",
-    },
-  ];
 
   return (
     <>
@@ -65,14 +52,13 @@ export default function Sidebar({
           </div>
 
           <div className="mt-4 space-y-1">
-            {Menus.map((Menu, index) => {
+            {Menus.map((Menu: any, index: number) => {
               const Icon = Icons[Menu.icon as keyof typeof Icons];
 
               return (
                 <Link
                   href={Menu.url}
                   key={Menu.url}
-
                   className={classNames(
                     path === Menu.url ? "bg-bfs_primary/20" : "",
                     `flex w-full gap-x-2 rounded-lg p-3 transition-colors duration-200 ease-in-out hover:bg-bfs_primary/20 
