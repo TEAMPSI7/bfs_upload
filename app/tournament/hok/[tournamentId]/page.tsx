dce81a81fb4a8cd9ddfcc296c972b37a8c6cd7c8
Jethro Cenas <jethrocenas@gmail.com>
2024-06-21 09:12:11 +0800

    changes

--- app/tournament/hok/[tournamentId]/page.tsx
+++ app/tournament/hok/[tournamentId]/page.tsx
@@ -1,18 +1,12 @@
-
-import React, { useEffect, useState } from 'react'
-import Link from "next/link";
-import axios from 'axios';
 import Image from 'next/image';
+import axios from 'axios';
 
 interface Participant {
-  teamName?: string;  // teamName is optional
+  teamName?: string;
   playerId: string;
   name: string;
   country: string;
-  // other properties of Participant
 }
-
-
 
 const fetchTournamentById = async (id: string) => {
   // Replace this with actual data fetching logic
@@ -29,29 +23,20 @@ const fetchAllTournamentIds = async () => {
 export async function generateStaticParams() {
   const tournaments = await fetchAllTournamentIds();
 
-  return tournaments.map((tournament : any) => ({
+  return tournaments.map((tournament: any) => ({
     tournamentId: tournament.id,
   }));
 }
 
+const TournamentPage = async ({ params }: { params: { tournamentId: string } }) => {
+  const { tournamentId } = params;
+  const tournament = await fetchTournamentById(tournamentId);
 
-const Page = () => {
-  const [participants, setParticipants] = useState<Participant[]>([]);
-  const [hoveredPlayerId, setHoveredPlayerId] = useState<string | null>(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          'https://script.google.com/macros/s/AKfycbyqDu6Q7OM7qwl5OqXWwk1HnutSlAXMMtPW-gV3WHj1S0EajsUqpRzuNmy7OL4XEv-g/exec'
-        );
-        console.log("RESPONSE ", response.data);
-        setParticipants(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
-  }, []);
+  if (!tournament) {
+    return <div>Tournament not found</div>;
+  }
+
+  const participants = tournament.participants as Participant[];
 
   const teams = participants.reduce((acc, participant) => {
     const teamName = participant.teamName || 'Unknown Team';
@@ -64,45 +49,41 @@ const Page = () => {
 
   return (
     <div className='relative h-full min-h-screen bg-cover pb-[2rem] bg-center bg-white/80'>
-      {/* <div className="absolute inset-0 bg-black opacity-50"></div> */}
       <div className="w-[90%] mx-auto  flex flex-col space-y-[1.5rem] overflow-hidden">
-      <div className='relative flex flex-col items-center mt-8 justify-center'>
-        <h1 className='text-7xl font-bold text-white' style={{ WebkitTextStroke: '1px black' }}>Honor of Kings</h1>
-        <h2 className='text-xl mt-2 font-bold text-white' style={{ WebkitTextStroke: '1px black' }}>5V5 ONLINE TOURNAMENT SEASON 1</h2>
-        <div className='w-full mt-[1.5rem]' style={{ 
-          backgroundImage: 'linear-gradient(90deg, rgba(0,0,0,0.8), rgba(255,255,255,0.8), rgba(0,0,0,0.8))',
-          color: '#FFFFFF',
-        }}>
-          <p className='text-white text-3xl text-center z-2 font-bold ' style={{ 
+        <div className='relative flex flex-col items-center mt-8 justify-center'>
+          <h1 className='text-7xl font-bold text-white' style={{ WebkitTextStroke: '1px black' }}>Honor of Kings</h1>
+          <h2 className='text-xl mt-2 font-bold text-white' style={{ WebkitTextStroke: '1px black' }}>5V5 ONLINE TOURNAMENT SEASON 1</h2>
+          <div className='w-full mt-[1.5rem]' style={{ 
+            backgroundImage: 'linear-gradient(90deg, rgba(0,0,0,0.8), rgba(255,255,255,0.8), rgba(0,0,0,0.8))',
             color: '#FFFFFF',
-            WebkitTextStroke: '1px black',
           }}>
-            PARTICIPANTS
-          </p>
+            <p className='text-white text-3xl text-center z-2 font-bold ' style={{ 
+              color: '#FFFFFF',
+              WebkitTextStroke: '1px black',
+            }}>
+              PARTICIPANTS
+            </p>
+          </div>
         </div>
-       
-      </div>
         <div className="relative p-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-5">
           {Object.keys(teams).map((teamName) => (
-           <div key={teamName} className='bg-white '>
+            <div key={teamName} className='bg-white '>
               <div className="relative h-[225px] rounded shadow group ">  
                 <div className="absolute inset-0"  
-                style={{
-                  backgroundImage: `url('/images/Honor of Kings/background.png')`,
-                  backgroundSize: '100% 100%',
-                  backgroundPosition: 'center',
-                  backgroundRepeat: 'no-repeat',
-            
-                }}
+                  style={{
+                    backgroundImage: `url('/images/Honor of Kings/background.png')`,
+                    backgroundSize: '100% 100%',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                  }}
                 >
                   <div className="absolute inset-0" style={{
-                  backgroundImage: ` url('/images/Honor of Kings/border.png')`,
-                  backgroundSize: '100% 100%',
-                  backgroundPosition: 'center',
-                  backgroundRepeat: 'no-repeat',
-            
+                    backgroundImage: ` url('/images/Honor of Kings/border.png')`,
+                    backgroundSize: '100% 100%',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
                   }}>
-                   {teamName != "VOIN ALLIANCE" ? <Image 
+                    {teamName !== "VOIN ALLIANCE" ? <Image 
                       src={`/images/HOK TEAMS/${teamName}.png`}
                       height={140}
                       width={140}
@@ -117,11 +98,8 @@ const Page = () => {
                       alt={teamName}
                       loading="lazy"
                       className=" absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                    />
-                    }
-
+                    />}
                   </div>
-                 
                 </div>
                 <div className="flex flex-col justify-start text-center z-10">
                   <div className="absolute inset-0 bg-black bg-opacity-50 hidden group-hover:block z-20"></div>
@@ -134,19 +112,18 @@ const Page = () => {
                     <div className="hidden group-hover:block justify-between bg-primary-dark bg-opacity-50 p-4">
                       {teams[teamName].slice(0, 5).map((player, index) => (
                         <div
-                        key={player.playerId + index}
-                        className="player-info text-accent flex space-x-2 border-b border-white"
-                        onMouseEnter={() => setHoveredPlayerId(player.playerId)}
-                        onMouseLeave={() => setHoveredPlayerId(null)}
-                        style={{
-                          backgroundImage: hoveredPlayerId === player.playerId
-                            ? 'linear-gradient(to right, rgba(255, 0, 0, 0.6), rgba(255, 255, 255, 0.6), rgba(255, 0, 0, 0.6))'
-                            : 'none',
-                        }}
-                      >
+                          key={player.playerId + index}
+                          className="player-info text-accent flex space-x-2 border-b border-white"
+                          // onMouseEnter={() => setHoveredPlayerId(player.playerId)}
+                          // onMouseLeave={() => setHoveredPlayerId(null)}
+                          // style={{
+                          //   backgroundImage: hoveredPlayerId === player.playerId
+                          //     ? 'linear-gradient(to right, rgba(255, 0, 0, 0.6), rgba(255, 255, 255, 0.6), rgba(255, 0, 0, 0.6))'
+                          //     : 'none',
+                          // }}
+                        >
                           <Image 
                             src={`/images/Flag Rectangle/${player.country}.png`}
-                            // src={'/images/Honor of Kings/EXTRACTION PH.jpg'}
                             width={20}
                             height={10}
                             className='h-4/5 self-center'
@@ -171,6 +148,6 @@ const Page = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Page;
+export default TournamentPage;
