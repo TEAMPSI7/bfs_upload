59a96a3246b9aed8def2fdbbda592a92612fc462
Jethro Cenas <jethrocenas@gmail.com>
2024-06-22 23:50:11 +0800

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,13 @@ const nextConfig = {
   output: 'export',
   distDir: 'out',  // Specify the output directory
   images: {
-      unoptimized: true,  // Necessary for static export
+    unoptimized: true,  // Necessary for static export
   },
   basePath: process.env.NODE_ENV === 'production' ? '/bfs_official' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/bfs_official/' : '',  // Adjust for GitHub Pages
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production' ? '/bfs_official' : '',
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
