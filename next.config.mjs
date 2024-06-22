4ce9f6c1628401d03f3bffb687b248789299d54e
Jethro Cenas <jethrocenas@gmail.com>
2024-06-22 22:59:08 +0800

    modified

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-    const nextConfig = {
-        output: 'export',  
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',  // Ensure the output directory is 'out'
+  images: {
+      unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',  // Set the asset prefix for GitHub Pages
 };
-  
-  export default nextConfig;
-  
+
+export default nextConfig;
