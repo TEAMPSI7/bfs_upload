59303877a9f64935d006872f52db334834b2043f
Jethro Cenas <cenasjethro@gmail.com>
2024-06-23 20:25:13 +0800

    Delete app directory

--- app/sponsorship/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <video className="h-fit w-full" autoPlay loop>
-      <source src="/videos/mountain-dew.mp4" type="video/mp4" />
-    </video>
-  );
-}
